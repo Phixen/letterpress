4ae4a30f1c4fcfe1c5c8d93f52bafba0ff32f8d8
Animesh Pathak <53110238+Sonichigo@users.noreply.github.com>
2025-01-29 13:22:53 +0530

    Update post.go

--- handler/post.go
+++ handler/post.go
@@ -48,6 +48,50 @@ func (h *Handler) GetPosts(c *gin.Context) {
 	}
 }
 
+func (h *Handler) CreatePost(c *gin.Context) {
+	var post models.Post
+	if err := c.ShouldBindJSON(&post); err != nil {
+		h.Logger.Err(err).Msg("could not parse request body")
+		c.JSON(http.StatusBadRequest, gin.H{"error": fmt.Sprintf("invalid request body: %s", err.Error())})
+		return
+	}
+	err := h.DB.SavePost(&post)
+	if err != nil {
+		h.Logger.Err(err).Msg("could not save post")
+		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("could not save post: %s", err.Error())})
+	} else {
+		c.JSON(http.StatusCreated, gin.H{"post": post})
+	}
+}
+
+func (h *Handler) UpdatePost(c *gin.Context) {
+	var id int
+	var post models.Post
+	var err error
+	if id, err = strconv.Atoi(c.Param("id")); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid post id"})
+		return
+	}
+	if err = c.ShouldBindJSON(&post); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": fmt.Sprintf("could not parse request: %s", err.Error())})
+		return
+	}
+
+	err = h.DB.UpdatePost(id, post)
+	if err != nil {
+		switch err {
+		case db.ErrNoRecord:
+			c.JSON(http.StatusNotFound, gin.H{"error": fmt.Sprintf("could not find post with id: %d", id)})
+		default:
+			h.Logger.Err(err).Msg("could not update post")
+			c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("could not update post: %s", err.Error())})
+		}
+		return
+	} else {
+		c.JSON(http.StatusOK, gin.H{"post": post})
+	}
+}
+
 func (h *Handler) GetPost(c *gin.Context) {
 	var id int
 	var err error
